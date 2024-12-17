393de0b74e93bfade13ed13753af15262cd407a0
Luke Williams <lukeawilliams@gmail.com>
2024-12-16 21:57:26 -0600

    Removing decayed birds

--- create-keps.ps1
+++ create-keps.ps1
@@ -7,7 +7,7 @@ if($BareTLE.StatusCode -ne 200)
     exit
 }
 
-$LinearSats = "RS-44","AO-07","FO-29","JO-97","XW-2B","MESAT1"
+$LinearSats = "RS-44","AO-07","FO-29","JO-97"
 $LinearKeps = ""
 foreach($Sat in $LinearSats)
 {
@@ -21,7 +21,7 @@ foreach($Sat in $LinearSats)
 }
 $LinearKeps | Out-File pub/linear.txt
 
-$FMSats = "AO-91","AO-92","ISS","PO-101","SO-50","Tevel-2","Tevel-3","Tevel-4","Tevel-7","Tevel-8","UVSQ-SAT","INSPIRE-Sat 7","LILACSAT-2"
+$FMSats = "AO-91","ISS","PO-101","SO-50","LILACSAT-2"
 $FMKeps = ""
 foreach($Sat in $FMSats)
 {
@@ -34,17 +34,9 @@ foreach($Sat in $FMSats)
     }
 }
 $FMKeps = $FMKeps.replace("AO-91","AO-91 FM 435.250 145.960 67.0")
-$FMKeps = $FMKeps.replace("AO-92","AO-92 FM 435.350 145.880 67.0")
 $FMKeps = $FMKeps.replace("ISS","ISS FM 145.990 437.800 67.0")
 $FMKeps = $FMKeps.replace("PO-101","PO-101 FM 437.500 145.900 141.3")
 $FMKeps = $FMKeps.replace("SO-50","SO-50 FM 145.850 436.795 67.0 2s 74.4")
-$FMKeps = $FMKeps.replace("Tevel-2","Tevel-2 FM 145.970 436.400")
-$FMKeps = $FMKeps.replace("Tevel-3","Tevel-3 FM 145.970 436.400")
-$FMKeps = $FMKeps.replace("Tevel-4","Tevel-4 FM 145.970 436.400")
-$FMKeps = $FMKeps.replace("Tevel-7","Tevel-7 FM 145.970 436.400")
-$FMKeps = $FMKeps.replace("Tevel-8","Tevel-8 FM 145.970 436.400")
-$FMKeps = $FMKeps.replace("UVSQ-SAT","UVSQ-SAT FM 145.905 437.020")
-$FMKeps = $FMKeps.replace("INSPIRE-Sat 7","INSPIRE-Sat 7 FM 145.970 437.410")
 $FMKeps = $FMKeps.replace("LILACSAT-2","LilacSat-2 (CAS-3H) FM 144.350 437.200")
 
 $FMKeps | Out-File pub/fm.txt
