056c28db559e2f5877dce8321ed2aaaacfdee29a
Luke Williams <lukeawilliams@gmail.com>
2025-07-07 11:25:15 -0500

    Adding Tevel2, Sonate-2, NO-44, and AO-73

--- create-keps.ps1
+++ create-keps.ps1
@@ -7,7 +7,7 @@ if($BareTLE.StatusCode -ne 200)
     exit
 }
 
-$LinearSats = "RS-44","AO-07","FO-29","JO-97"
+$LinearSats = "RS-44","AO-07","FO-29","JO-97","AO-73"
 $LinearKeps = ""
 foreach($Sat in $LinearSats)
 {
@@ -21,7 +21,7 @@ foreach($Sat in $LinearSats)
 }
 $LinearKeps | Out-File pub/linear.txt
 
-$FMSats = "AO-91","ISS","PO-101","SO-50","LILACSAT-2"
+$FMSats = "AO-91","ISS","PO-101","SO-50","LILACSAT-2","TEVEL2-1","TEVEL2-2","TEVEL2-3","TEVEL2-4","TEVEL2-5","TEVEL2-6","TEVEL2-7","TEVEL2-8","HADES-R (SO-124)","HADES-ICM (SO-125)"
 $FMKeps = ""
 foreach($Sat in $FMSats)
 {
@@ -38,10 +38,20 @@ $FMKeps = $FMKeps.replace("ISS","ISS FM 145.990 437.800 67.0")
 $FMKeps = $FMKeps.replace("PO-101","PO-101 FM 437.500 145.900 141.3")
 $FMKeps = $FMKeps.replace("SO-50","SO-50 FM 145.850 436.795 67.0 2s 74.4")
 $FMKeps = $FMKeps.replace("LILACSAT-2","LilacSat-2 (CAS-3H) FM 144.350 437.200")
+$FMKeps = $FMKeps.replace("TEVEL2-1","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-2","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-3","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-4","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-5","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-6","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-7","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("TEVEL2-8","Tevel2-1 FM 145.970 436.400")
+$FMKeps = $FMKeps.replace("HADES-R (SO-124)","HADES-R (SO-124) FM 145.925 436.885")
+$FMKeps = $FMKeps.replace("HADES-ICM (SO-125)","HADES-ICM (SO-125) FM 145.875 436.666")
 
 $FMKeps | Out-File pub/fm.txt
 
-$DigiSats = "IO-117","LEDSAT","NO-44"
+$DigiSats = "IO-117","LEDSAT","NO-44","SONATE-2"
 $DigiKeps = ""
 foreach($Sat in $DigiSats)
 {
@@ -53,4 +63,6 @@ foreach($Sat in $DigiSats)
         Write-Warning "No match found for $Sat"
     }
 }
+$DigiKeps = $DigiKeps.replace("SONATE-2","SONATE-2 APRS 145.825"
+
 $DigiKeps | Out-File pub/digi.txt
